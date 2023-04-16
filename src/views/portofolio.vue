da00f2032bbde974ab142ba881663e7206720627
Nabila Al Rasyid <nabilaalrasyid@gmail.com>
2023-04-16 23:03:01 +0700

    comment

--- src/views/portofolio.vue
+++ src/views/portofolio.vue
@@ -1,10 +1,12 @@
 <template>
+  <div class="porto">
   <h4> Portofolio </h4>
   <div class="link">
   <a href="https://tirastimes.com/pucuk-kata-puisi-nabila-al-rasyid-menyerah/2021/01/31/"> Check This Page </a>
   </div>
 
   <p> I'm a good empath. Thinking too much make my soul always feeling tired. So, i make a bunch of words for distraction this negative feeling. One of my hobbies are make some poems. This is my first publication poem. My hobby of make some poems rise from this. </p>
+  </div>
 </template>
 
 <script>
@@ -16,6 +18,11 @@ export default {
 </script>
 
 <style>
+.porto {
+  background-color: lightgray;
+  padding-bottom: 30px;
+}
+
 h4 {
 padding-top: 130px;
   font-family: "Lucida Bright";
